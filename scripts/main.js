1949650f7be160103d6d39a91817e153b55e16be
Roma <romakoder@gmail.com>
2020-02-06 20:35:28 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const tree = document.querySelector('.tree');
+const list = tree.querySelectorAll('li');
+
+for (const li of list) {
+  const span = document.createElement('span');
+
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.onclick = function(e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ulContainer = e.target.parentNode.querySelector('ul');
+
+  if (!ulContainer) {
+    return;
+  }
+
+  ulContainer.hidden = !ulContainer.hidden;
+};
